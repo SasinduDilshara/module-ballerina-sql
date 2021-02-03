7dafb3cdd12d09c10a25ee26545c8a73720c2bc6
sasndu <dilsharasasindu@gmail.com>
2021-02-03 18:33:24 +0530

    change the parameter order of statementparameterprocessor and change the date in header

--- sql-native/src/main/java/org/ballerinalang/sql/nativeimpl/CallProcessor.java
+++ sql-native/src/main/java/org/ballerinalang/sql/nativeimpl/CallProcessor.java
@@ -1,3 +1,21 @@
+/*
+ *  Copyright (c) 2021, WSO2 Inc. (http://www.wso2.org) All Rights Reserved.
+ *
+ *  WSO2 Inc. licenses this file to you under the Apache License,
+ *  Version 2.0 (the "License"); you may not use this file except
+ *  in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *  http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing,
+ *  software distributed under the License is distributed on an
+ *  "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ *  KIND, either express or implied.  See the License for the
+ *  specific language governing permissions and limitations
+ *  under the License.
+ */
+
 package org.ballerinalang.sql.nativeimpl;
 
 import io.ballerina.runtime.api.TypeTags;
@@ -165,7 +183,7 @@ public class CallProcessor {
                     case Constants.ParameterObject.INOUT_PARAMETER:
                         Object innerObject = objectValue.get(Constants.ParameterObject.IN_VALUE_FIELD);
                         sqlType = statementParameterProcessor.setSQLValueParam(connection, statement, 
-                                             innerObject, index, true);
+                                            index, innerObject, true);
                         outputParamTypes.put(index, sqlType);
                         statement.registerOutParameter(index, sqlType);
                         break;
@@ -175,10 +193,10 @@ public class CallProcessor {
                         statement.registerOutParameter(index, sqlType);
                         break;
                     default:
-                        statementParameterProcessor.setSQLValueParam(connection, statement, object, index, false);
+                        statementParameterProcessor.setSQLValueParam(connection, statement, index, object, false);
                 }
             } else {
-                statementParameterProcessor.setSQLValueParam(connection, statement, object, index, false);
+                statementParameterProcessor.setSQLValueParam(connection, statement, index, object, false);
             }
         }
     }
